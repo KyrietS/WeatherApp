3bd0b95475d490a9d60d45756e2ea68d953b3232
Sebastian Fojcik <fojcik.sebastian@gmail.com>
2019-04-08 15:56:06 +0200

    Dodano mockupowy obiekt JSON
    Aby nie zużywać API

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(async function(){
+
+    let apiKey = 'q4yfgRGEhyRygj2fDKAZA02kiOUDkaPP'
+    let locationId = '273125';
+    let additions = '&details=true&metric=true';
+
+    // let response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationId}?apikey=${apiKey}${additions}`);
+    // let data = await response.json();
+    // let str = JSON.stringify(data);
+    // console.log(data);
+    // document.querySelector('#json').innerHTML = str;
+
+    // let x = mockupResponse.Headline.Text;
+
+    console.log(mockupResponse.Headline.Text);
+
+})()
