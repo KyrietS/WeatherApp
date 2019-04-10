82ffccbfb2872d91a1b84824ee6f95873056d445
KrissW8 <47832021+KrissW8@users.noreply.github.com>
2019-04-10 20:06:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+﻿// Ikony tylko dla dnia
+// https://developer.accuweather.com/weather-icons
+var icon = {
+    1: 'icon-sun',
+    2: 'icon-sun',
+    3: 'icon-cloud-sun',
+    4: 'icon-cloud-sun',
+    5: 'icon-fog-sun',
+    6: 'icon-cloud-sun',
+    7: 'icon-clouds',
+    8: 'icon-clouds',
+    9: 'icon-na',
+    10: 'icon-na',
+    11: 'icon-fog',
+    12: 'icon-rain',
+    13: 'icon-rain',
+    14: 'icon-rain',
+    15: 'icon-hail',
+    16: 'icon-hail',
+    17: 'icon-hail',
+    18: 'icon-hail',
+    19: 'icon-windy-rain',
+    20: 'icon-windy-rain',
+    20: 'icon-windy-rain',
+    21: 'icon-windy-rain',
+    22: 'icon-snow',
+    23: 'icon-snow-heavy',
+    24: 'icon-snowflake',
+    25: 'icon-rain',
+    26: 'icon-rain',
+    27: 'icon-na',
+    28: 'icon-na',
+    29: 'icon-rain',
+    30: 'icon-temperature',
+    31: 'icon-snowflake',
+    32: 'icon-wind',
+}
+
+// Funkcja uzupełnia informację dla danego dnia
+function fillDayInfo( dayIndex, data ) {
+
+    // Złapanie odpowiedniego div'a z dniem
+    const day = document.querySelectorAll(".day")[dayIndex];
+    // Złapanie odpowiedniego obiektu JSON z dniem
+    const forecast = data.DailyForecasts[dayIndex];
+
+    // Nazwa dnia
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const date = new Date(forecast.Date);
+    const dayName = dayIndex == 0 ? "Today" : days[date.getDay()];
+    day.querySelector('h2').textContent = dayName;
+
+    // Temperatura
+    const temperature = forecast.Temperature.Maximum.Value;
+    day.querySelector('.temperature').textContent = Math.round(temperature) + '°C';
+    
+    // Ikona
+    const iconId = forecast.Day.Icon;
+    day.querySelector('.icon').className = 'icon ' + icon[iconId];
+
+    // Opis
+    day.querySelector('.forecast').textContent = forecast.Day.ShortPhrase;
+}
+
+// Funkcja uruchamiana przy starcie strony, pobiera 
+// dane (tymczasowo mockupResponse) i uzupełnia poszczególne dni informacjami
+(async function(){
+
+    let apiKey = 'q4yfgRGEhyRygj2fDKAZA02kiOUDkaPP'
+    let locationId = '273125';
+    let additions = '&details=true&metric=true';
+
+    // let response = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationId}?apikey=${apiKey}${additions}`);
+    // let data = await response.json();
+    // let str = JSON.stringify(data);
+    // console.log(data);
+    // document.querySelector('#json').innerHTML = str;
+
+    // let x = mockupResponse.Headline.Text;
+
+    for( let i = 0; i < 5; i++ ) {
+        fillDayInfo( i, mockupResponse );
+    }
+})()
+
+let locationId = '273125';
+const getLocation = (e) => {
+    let apiKey = 'drOPfTFPJtMpmZP8HhGBAmXmfx5wMytH'
+    let additions = '&details=true&metric=true';
+    const city = document.querySelector("#input-location").value;
+    const gl = async () => {
+        let response = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`);
+        let data = await response.json();
+        try {
+             locationId = data[0].Key;
+        } catch (error) {
+            console.log('Wrong city')
+        }
+    }
+    gl()
+    e.preventDefault();
+}
+
+document.querySelector("#input-location").addEventListener("input", getLocation);
